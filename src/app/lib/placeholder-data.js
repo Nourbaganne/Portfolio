10453780a23bfcaa478c6a4864338f888808572c
Nourbaganne <bagannenour007@gmail.com>
2023-12-26 00:06:50 +0100

    add placeholder data and data type definitions

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,22 @@
+// This file contains placeholder data
+
+const experience = [
+    {
+      id: '0',
+      companyName: 'Retain Health',
+      role: 'Software Engineer',
+      startDate: new Date(2022, 7),
+      endDate: new Date(),
+      description: "During my tenure at Retain Health as a software engineer, I've been integral to the design and development of innovative software features, employing a tech stack that includes AngularJS, ExpressJS, Bootstrap, and MySQL. My responsibilities extend beyond development, encompassing the identification, reporting, and resolution of issues encountered in the software lifecycle. This commitment to issue resolution is complemented by my proficiency in designing, developing, and executing automated tests using Javascript test frameworks. This comprehensive approach ensures the delivery of robust and high-quality software, reflecting my dedication to excellence in both feature development and software reliability.",
+      Skills: ['AngularJs', 'ExpressJs', 'MySQL', 'Protractor', 'Automation Testing']
+    },
+    {
+        id: '1',
+        companyName: 'ARSII Association',
+        role: 'Front End Development Intern',
+        startDate: new Date(2023, 8),
+        endDate: new Date(2023, 10),
+        description: "During my internship at ARSII Association, I played a pivotal role in the development of af dynamic user interface through the creation of an Angular-based admin dashboard. This project involved not only coding proficiency but also required seamless integration of a teammate's API to enhance the overall functionality of the dashboard. A key aspect of my role involved close collaboration with cross-functional teams to ensure effective communication and the smooth integration between front-end and back-end systems. In addition to functionality, I prioritized user experience by crafting responsive interfaces, optimizing performance for diverse devices. This experience allowed me to hone my technical skills, foster effective teamwork, and contribute to the development of a comprehensive and user-friendly system.",
+        Skills: ['Angular2+', 'Swagger', 'MySQL', 'Bootstrap']
+      },
+];
--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -4,20 +4,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] z-50 px-5">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <div className="h-auto w-auto flex flex-row items-center">
-          <span className="font-bold ml-[10px] hidden lg:block text-gray-300">
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             <Image
               src="/logo.png"
               width={200}
-              height={500}
+              height={100}
               alt="Logo Here"
             />
           </span>
         </div>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="w-[500px] h-full flex flex-row items-center justify-between">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-50">
             <a href="#about-me" className="cursor-pointer hover:text-gray-300 hover:translate-y-1">
               About me
