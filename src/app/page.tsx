0eac9813a75f56034975db29109c691e458f6001
Nourbaganne <bagannenour007@gmail.com>
2023-12-25 01:19:12 +0100

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
 import Image from 'next/image'
-import Navbar from './ui/navbar/navbar'
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar></Navbar>
-    </main>
+    <>
+      <main className='z-20 relative'>
+          <div className=' max-w-6xl m-auto min-h-screen sm:min-h-[1000px] flex flex-col items-center justify-center gap-8'>
+            <h1 className="text-3xl md:text-7xl text-center font-extrabold bg-gradient-to-r text-transparent from-[#95dff1] to-[#3F0071] bg-clip-text">
+              Software engineer &
+              <br className='hidden sm:block'/>
+              Full stack JavaScript Developer
+            </h1>
+            <div className='text-[#B5B4B5] text-sm sm:text-base font-light max-w-[90%] sm:max-w-2xl text-center'>
+              <p><span className='font-extrabold'>Greetings!</span> I&apos;m Nour Baganne, an enthusiastic <span className='font-bold'>software engineer</span> and <span className='font-bold'>QA Tester</span> deeply immersed in the world of technology.</p> 
+              <br />
+
+              <p>My expertise lies in crafting robust and efficient solutions using a <span className='font-bold'>full-stack JavaScript</span> approach, with a particular fondness for <span className='font-bold'>React</span>, <span className='font-bold'>Node.js</span>, and <span className='font-bold'>Next.js</span>. </p>
+              <br />
+
+              <p>Currently thriving in my role as a <span className='font-bold'>full-stack software developer</span> at <span className='font-bold'>Retain Health</span>, I consider JavaScript not just a language but my creative playground.</p>
+              <br />
+              <p>
+                Driven by a passion for innovation, I specialize in developing <span className='font-bold'>high-performance applications</span> that push the boundaries of what is possible. I am always eager to explore new opportunities and tackle fresh challenges in the dynamic realm of software engineering. <a href="#" className='underline decoration-2 font-bold'>Let&apos;s connect</a> and bring your ideas to life through the power of code!
+                </p>
+            </div>
+            <div className='flex flex-row'>
+              <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm py-2.5 text-center me-2 mb-2 w-32 sm:w-48">Github</button>
+              <button className="inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-light rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 text-white">
+                <span className="py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0 text-lg w-32 sm:w-48">
+                Resume
+                </span>
+              </button>
+            </div>
+          </div>
+          <hr className='mx-24'/>  
+          
+      </main>
+    </>
   )
 }
--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -1,15 +1,21 @@
 // import { Socials } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
+        <div className="h-auto w-auto flex flex-row items-center">
           <span className="font-bold ml-[10px] hidden lg:block text-gray-300">
-            Nour Baganne
+            <Image
+              src="/logo.png"
+              width={200}
+              height={500}
+              alt="Logo Here"
+            />
           </span>
-        </a>
+        </div>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-50">
